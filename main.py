11190ab98530fb371efc654b20148b22fa0124bc
cauafreitas7 <128155396+cauafreitas7@users.noreply.github.com>
2023-04-19 19:36:18 -0300

    Atualizar o main.py
    feito

---
+++ main.py
@@ -0,0 +1,26 @@
+a = int(input('Digite um número a: '))
+b = int(input('Digite um numero b: '))
+c = int(input('Digite um numero c: '))
+
+x = (c + b > a)
+y = (a + c > b)
+z = (a + b > c)
+print('x')
+print('y')
+print('z')
+
+
+if: x==False
+   print('Não é um triângulo')
+elif: y==False 
+   print('Não é um triângulo')
+elif: z==False 
+   print('Não é um triângulo')
+
+else:
+   if a == b and b == c and c == a
+      print('Equilátero')
+   if a != b and b != c and c != a
+      print('Escaleno')
+   if (a==b!=c) and (a==c!=b) and (b==c!=a)
+      print('Isóceles')
