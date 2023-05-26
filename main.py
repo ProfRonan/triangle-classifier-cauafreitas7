1e6baca511e1c6209a6cd5632198e6e65f467ed8
cauafreitas7 <128155396+cauafreitas7@users.noreply.github.com>
2023-05-26 13:36:37 +0000

    por causa de um 's'??

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ if c + b > a and a + c > b and a + b > c:
       if a != b and b != c and c != a:
             print('Escaleno')
       if a == b and b != c or a == c and c != b or b == c and c != a:
-            print('Isóceles')
+            print('Isósceles')
 else:
       print('Não é um triângulo') 
